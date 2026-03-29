71abf7ec68907bb162f0e4377b5ce46292830abc
Rajat <rajatpoddar17@gmail.com>
2026-03-29 23:26:26 +0530

    update docker files and ports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import {
 
 import MenuCard from './MenuCard';
 
-const API_BASE_URL = `http://${window.location.hostname}:5000`;
+const API_BASE_URL = `http://${window.location.hostname}:5534`;
 
 
 // --- MAIN APP WRAPPER WITH THEME & AUTH ---
